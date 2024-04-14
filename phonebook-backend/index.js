cdb250822214107f1a2f8c5eb40b55d2db200181
Gerald <gpserrano.18.4@gmail.com>
2024-04-13 20:14:20 -0500

    Add 3.19 exercise

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -13,10 +13,12 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
+
 app.use(cors())
 
 app.use(express.static('dist'))
@@ -100,21 +102,16 @@ app.delete('/api/persons/:id', (request, response,next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const {name, number} = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, {name, number}, { new: true, runValidators: true, context: 'query' })
     .then(updatedNumber => {
       response.json(updatedNumber)
     })
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response,next) => {
   const body = request.body
 
   if (!body.name) {
@@ -136,7 +133,7 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(personSaved =>{
     response.json(personSaved)
-  })
+  }).catch(error => next(error))
 })
 
 
